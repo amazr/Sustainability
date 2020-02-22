99f5f54bda07dcf654556ebe0692b2b5da5b86b3
AlexLaptop <alma9011@colorado.edu>
2020-02-22 13:48:50 -0700

    Added test POST command, setup mongo

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
+//Requirements
 const express = require('express');
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 
+//view engine
 app.set('view engine', 'ejs');
+//Declaring body parser for reading data from html
+app.use(bodyParser.urlencoded({extended: true}));
 
+//Connecting to the mongodb
+MongoClient.connect("mongodb+srv://alexUser:KnockKnock123@cluster0-knqsw.gcp.mongodb.net/test?retryWrites=true&w=majority", (err, database) => {
+    if (err) console.log(err);
+    //Connect to our cluster
+    db = database.db('Cluster0');
+
+    app.listen(8080, () => {
+        console.log("Server running on localhost:8080");
+    });
+    
+});
+
+//GET requests
 app.get('/', (req, res) => {
   res.render('pages/index.ejs');
 });
 
-const server = app.listen(8080, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log(`Example app listening at http://${host}:${port}`);
+//POST requests
+app.post('/test', (req,res) =>{
+    console.log(req.body);
 });
+
+//Server plz turn on
+//const server = app.listen(8080, () => {
+//  console.log(`Example app listening at http://${host}:${port}`);
+//});
